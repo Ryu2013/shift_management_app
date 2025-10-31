daa3f2582f9f6736cb9addf8eaf3745f54ca507d
ryu <kaigo2013.ryuuiti@gmail.com>
2025-10-31 20:30:03 +0900

    UseのUpdate時、パスワードとEmailの変更以外をパスワードなしで可能に変更”

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,6 +6,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
    before_action :office_authenticate, only: [ :edit, :update, :destroy ]
    before_action :set_team, only: [ :edit, :update ]
 
+  protected
+  def update_resource(resource, params)
+
+  email_changed = params.key?(:email) && params[:email] != resource.email
+    if params[:password].present? || email_changed
+    super
+    else
+    # プロフィール系のみを current_password なしで更新
+    resource.update_without_password(
+    params.except(:current_password, :password, :password_confirmation, :email)
+    )
+    end
+  end
+
+  
   private
   # New,Create時に@officeをセット。session[:office_id]がなければリダイレクト
   def set_office_from_session
@@ -27,6 +42,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
       :name, :address, :pref_per_week, :commute, :team_id,
       :email, :password, :password_confirmation, :current_password
     )
+  end
+
+  #　User情報更新時にパスワードとEmail以外の変更はcurrent_passwordなしで更新可能にする
+  def update_resource(resource, params)
+    email_changed = params.key?(:email) && params[:email] != resource.email
+      if params[:password].present? || email_changed
+      super
+      else
+      # プロフィール系のみを current_password なしで更新
+      resource.update_without_password(
+      params.except(:current_password, :password, :password_confirmation, :email)
+      )
+      end
   end
 
   def office_authenticate
