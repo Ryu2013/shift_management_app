600f434c0c8b41bb01acb3667956c733dd387a81
ryu <kaigo2013.ryuuiti@gmail.com>
2025-11-16 17:13:34 +0900

    officeにdependent: :destroyのテストを追加。ブロードキャスト側でnilエラーが出てしまうので、各ブロードキャストでガードを追記

--- app/models/client_need.rb
+++ app/models/client_need.rb
@@ -8,14 +8,14 @@ class ClientNeed < ApplicationRecord
   enum shift_type: { day: 0, night: 1 }
   enum week: { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
 
-  after_create_commit  -> { broadcast_append_to  stream_key, target: "client_needs_#{week}" }
-  after_update_commit  -> { broadcast_replace_to stream_key }
-  after_destroy_commit -> { broadcast_remove_to  stream_key }
+  after_create_commit  -> { broadcast_append_to  stream_key, target: "client_needs_#{week}" if stream_key }
+  after_update_commit  -> { broadcast_replace_to stream_key if stream_key }
+  after_destroy_commit -> { broadcast_remove_to  stream_key if stream_key }
 
   private
   def set_office_id
     self.office_id = client.office_id
   end
 
-  def stream_key = [ client.team, :client_needs ]
+  def stream_key = [ client&.team, :client_needs ]
 end
