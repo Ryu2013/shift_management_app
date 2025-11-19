30d86ab0842bbb731c0d8101b6a8b079c5878d41
ryu <kaigo2013.ryuuiti@gmail.com>
2025-11-19 22:17:21 +0900

    Css調整、ログアウト先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # deviseのログアウト後のリダイレクト先を指定
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   protected
 
   # ログイン時に二段階認証コードを許可
