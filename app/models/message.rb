b98777ae14cb4e1d92c6c85b4a88eb45262389c7
ryu <kaigo2013.ryuuiti@gmail.com>
2025-12-08 22:29:10 +0900

    メッセージのブロードキャスト機能を追加し、ルームの表示を部分テンプレートに移動

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,8 +5,15 @@ class Message < ApplicationRecord
   validates :content, presence: true
   before_validation :set_office_id
   after_create_commit { broadcast_append_to room }
+  after_create_commit :broadcast_room_update
 
   private
+
+  def broadcast_room_update
+    room.users.each do |user|
+      broadcast_replace_to user, target: "room_#{room.id}", partial: "rooms/room", locals: { room: room, current_user: user }
+    end
+  end
 
   def set_office_id
     self.office_id = room.office_id
